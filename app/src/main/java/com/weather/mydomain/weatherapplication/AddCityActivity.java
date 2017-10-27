77e412df70d2563406b22b34d3fb044777518f10
anamwa <matouk.ma@gmail.com>
2017-10-27 10:47:30 +0200

    Add button done with mipmap

--- app/src/main/java/com/weather/mydomain/weatherapplication/AddCityActivity.java
+++ app/src/main/java/com/weather/mydomain/weatherapplication/AddCityActivity.java
@@ -1,6 +1,5 @@
 package com.weather.mydomain.weatherapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
